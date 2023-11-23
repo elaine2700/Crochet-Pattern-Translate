ded8fa442bbde16b3fc70941d293b30f536bc2d6
elaine2700 <easerrano.e@gmail.com>
2023-11-22 19:43:32 -0800

    Styled Contact Page

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -3,18 +3,43 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='contact-section section-container'>
-      <div className='contact-card'>
-        <p>Contact us here</p>
-        <a>email@email.com</a>
-      </div>
-      <div className='contact-card'>
-      <p>Contribute. Send us a message here</p>
-        <a>email@email.com</a>
-      </div>
-      <div className='contact-card'>
-        <p>Donate to keep this database alive</p>
-        <button className='btn-primary'>Donate</button>
-      </div>
+      <section className='contact-card'>
+        <header>
+          <h2>Contact</h2>
+        </header>
+        <div className='contact-card-content'>
+          <p className='contact-card-paragraph'>Contact us here</p>
+          <div className='contact-card-cta'>
+            <a>contact@crochetspacecraft.com</a>
+          </div>
+        </div>
+        
+      </section>
+      <section className='contact-card middle'>
+        <header>
+          <h2>Contribute</h2>
+        </header>
+        <div className='contact-card-content'>
+          <p className='contact-card-paragraph'>Contribute. Send us a message here</p>
+          <div className='contact-card-cta'>
+            <a>contribute@crochetspacecraft.com</a>
+          </div>
+        </div>
+        
+      </section>
+      <section className='contact-card'>
+        <header>
+          <h2>Donate</h2>
+        </header>
+        <div className='contact-card-content'>
+          <p className='contact-card-paragraph'>Donate to keep this database alive</p>
+          <div className='contact-card-cta'>
+            <button className='btn-primary'>Donate</button>
+          </div>
+          
+        </div>
+        
+      </section>
     </div>
   )
 }
