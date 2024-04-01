4f391c34940b36d0292ba5bb5146eca8eb9db083
elaine2700 <easerrano.e@gmail.com>
2024-03-31 22:50:07 -0600

    Deploying fuctions

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const PORT = 3030;
-const mongoDbUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_KEY}@cluster0.emdprjk.mongodb.net/?retryWrites=true&w=majority`;
-
-
-module.exports = {PORT, mongoDbUri};
