1af1eeebc396a85128d8dd37be5f3cb33b918df7
elaine2700 <easerrano.e@gmail.com>
2024-08-11 22:50:53 -0600

    Add subtitle to Gallery Cards

--- Project/src/Components/Gallery Card/GalleryCard.jsx
+++ Project/src/Components/Gallery Card/GalleryCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import cardStyles from './gallerycard.module.css'
 
-const GalleryCard = ({ cardName = 'Stitch', linkTo, imagePath = 'dog-bear.jpg' }) => {
+const GalleryCard = ({ cardName = 'Stitch', linkTo, imagePath = 'dog-bear.jpg', cardSubtitle='Subtitle' }) => {
     const [hoverState, setHoverState] = useState(false);
 
     const handleHoverState = (e, enter) => {
@@ -19,7 +19,7 @@ const GalleryCard = ({ cardName = 'Stitch', linkTo, imagePath = 'dog-bear.jpg' }
                 !hoverState ?
                     (<div className={cardStyles.details}>
                         <h2>{cardName}</h2>
-                        <p>Basic</p>
+                        <p>{cardSubtitle}</p>
                     </div>) :
                     (<div className={`${cardStyles.details} ${cardStyles.back}`}>
                         
